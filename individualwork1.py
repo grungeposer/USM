1990c02b9a188a0c6e988af4e254439d0edd58dd
grungeposer <159921550+grungeposer@users.noreply.github.com>
2024-02-17 10:31:44 +0300

    individualwork1.py

--- individualwork1.py
+++ individualwork1.py
@@ -9,3 +9,6 @@ nums = [0, 42, 21, 0, 100, 0, 5, 1, 0, 7, 3, 0, 404, 0]
 new_len = remove_zeroes(nums)
 print(nums[:new_len])
 print(new_len)
+
+assert new_len == 8
+assert nums[:new_len] == [42,21,100,5,1,7,3,404]
