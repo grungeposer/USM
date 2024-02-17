fc5a1be5199094c15036d245196c06380c35b2c1
grungeposer <159921550+grungeposer@users.noreply.github.com>
2024-02-17 09:48:42 +0300

    Add files via upload

---
+++ individualwork1.py
@@ -0,0 +1,10 @@
+def remove_zeroes(numbers: list[int]) -> int:
+    non_zero_index = 0
+    for i in range(len(numbers)):
+        if numbers[i] != 0:
+            numbers[non_zero_index], numbers[i] = numbers[i], numbers[non_zero_index]
+            non_zero_index += 1
+    return non_zero_index
+nums = [0, 42, 21, 0, 100, 0, 5, 1, 0, 7, 3, 0, 404, 0]
+new_len = remove_zeroes(nums)
+print(nums[:new_len])
